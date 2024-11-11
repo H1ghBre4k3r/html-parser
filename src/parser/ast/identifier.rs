4dc0b42f7b62b829a36b6ef886285b88144d6a1b
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-11-11 19:00:26 +0100

    feat(tests): add tests for ParsedIdentifier

--- src/parser/ast/identifier.rs
+++ src/parser/ast/identifier.rs
@@ -23,3 +23,56 @@ impl Parseable for ParsedIdentifier {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use lachs::Span;
+
+    use crate::{AstNode, LAngle, ParseError, ParseStream, Parseable, Token, TokenKind};
+
+    use super::ParsedIdentifier;
+
+    fn stream(input: &str) -> ParseStream {
+        let tokens = Token::lex(input).unwrap();
+
+        ParseStream::new(tokens)
+    }
+
+    #[test]
+    fn test_ident() {
+        let mut tokens = stream("foo");
+        let result = ParsedIdentifier::try_parse(&mut tokens);
+
+        assert_eq!(
+            result,
+            Ok(AstNode::Identifier(ParsedIdentifier {
+                value: "foo".into(),
+                position: Span::default()
+            }))
+        );
+    }
+
+    #[test]
+    fn test_non_ident() {
+        let mut tokens = stream("<foo");
+        let result = ParsedIdentifier::try_parse(&mut tokens);
+
+        assert_eq!(
+            result,
+            Err(ParseError::Mismatch(
+                TokenKind::Identifier,
+                Token::LAngle(LAngle {
+                    position: Span::default()
+                })
+            ))
+        );
+    }
+
+    #[test]
+    fn test_eof() {
+        let mut tokens = stream("");
+        let result = ParsedIdentifier::try_parse(&mut tokens);
+
+        assert_eq!(result, Err(ParseError::Eof));
+    }
+}
