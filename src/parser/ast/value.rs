2a20a7888643dc544ebd9fad9338f1bc48e0d922
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-11-11 23:26:07 +0100

    feat(tests): add tests for ParsedValue

--- src/parser/ast/value.rs
+++ src/parser/ast/value.rs
@@ -24,3 +24,56 @@ impl Parseable for ParsedValue {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use lachs::Span;
+
+    use crate::{
+        AstNode, LAngle, ParseError, ParseStream, Parseable, ParsedValue, Token, TokenKind,
+    };
+
+    fn stream(input: &str) -> ParseStream {
+        let tokens = Token::lex(input).unwrap();
+
+        ParseStream::new(tokens)
+    }
+
+    #[test]
+    fn test_ident() {
+        let mut tokens = stream(r#""foo""#);
+        let result = ParsedValue::try_parse(&mut tokens);
+
+        assert_eq!(
+            result,
+            Ok(AstNode::Value(ParsedValue {
+                value: "foo".into(),
+                position: Span::default()
+            }))
+        );
+    }
+
+    #[test]
+    fn test_non_ident() {
+        let mut tokens = stream(r#"<"foo""#);
+        let result = ParsedValue::try_parse(&mut tokens);
+
+        assert_eq!(
+            result,
+            Err(ParseError::Mismatch(
+                TokenKind::Value,
+                Token::LAngle(LAngle {
+                    position: Span::default()
+                })
+            ))
+        );
+    }
+
+    #[test]
+    fn test_eof() {
+        let mut tokens = stream("");
+        let result = ParsedValue::try_parse(&mut tokens);
+
+        assert_eq!(result, Err(ParseError::Eof));
+    }
+}
